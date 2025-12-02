e7b104e5825c8c7f37e072b5a7a065e189577a0a
Christopher Kaster <me@atomicptr.de>
2025-12-02 10:23:57 +0100

    add 2025-01 part 2

--- 2025/01/main.rs
+++ 2025/01/main.rs
@@ -4,8 +4,13 @@ fn main() {
 
     println!(
         "Part 1 result is: {}",
-        count_state_reached(Safe(50), rots, 0)
+        count_state_reached(Safe::new(50), &rots, 0)
     );
+
+    println!(
+        "Part 2 result is: {}",
+        Safe::new(50).apply_rotations(&rots).clicks
+    )
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -40,9 +45,23 @@ pub fn parse_rot_seq(input: &str) -> Vec<Rot> {
     input.lines().map(|line| line.to_string().into()).collect()
 }
 
-pub struct Safe(pub u8);
+pub struct Safe {
+    pub current: u8,
+    pub clicks: u32,
+}
 
 impl Safe {
+    pub fn new(current: u8) -> Self {
+        Safe { current, clicks: 0 }
+    }
+
+    pub fn with_clicks(self, clicks: u32) -> Safe {
+        Safe {
+            current: self.current,
+            clicks,
+        }
+    }
+
     pub fn apply_rotation(self, rot: Rot) -> Self {
         match rot {
             Rot::Left(val) => self.turn(-i16::from(val)),
@@ -50,26 +69,36 @@ impl Safe {
         }
     }
 
-    fn turn(self, val: i16) -> Self {
-        Safe(
-            (self.0 as i16 + val as i16)
-                .rem_euclid(100)
-                .try_into()
-                .expect("invalid state"),
-        )
+    pub fn apply_rotations(self, rots: &Vec<Rot>) -> Self {
+        rots.iter()
+            .cloned()
+            .fold(self, |safe, rot| safe.apply_rotation(rot))
     }
 
-    pub fn value(&self) -> u8 {
-        self.0
+    fn turn(self, val: i16) -> Self {
+        let mut current = self.current as i16;
+        let mut clicks = self.clicks;
+
+        let dir = if val >= 0 { 1 } else { -1 };
+
+        for _ in 0..val.abs() {
+            current = (current + dir).rem_euclid(100);
+
+            if current == 0 {
+                clicks += 1;
+            }
+        }
+
+        Safe::new(u8::try_from(current).expect("invalid state")).with_clicks(clicks)
     }
 }
 
-fn count_state_reached(safe: Safe, rots: Vec<Rot>, value: u8) -> u32 {
+fn count_state_reached(safe: Safe, rots: &Vec<Rot>, value: u8) -> u32 {
     rots.iter()
         .cloned()
         .fold((safe, 0), |(safe, curr), rot| {
             let safe = safe.apply_rotation(rot);
-            let v = safe.0;
+            let v = safe.current;
 
             (safe, if v == value { curr + 1 } else { curr })
         })
@@ -120,24 +149,37 @@ L82"#;
 
     #[test]
     fn test_safe_apply_rotations() {
-        assert_eq!(19, Safe(11).apply_rotation(Rot::Right(8)).value());
-        assert_eq!(5, Safe(11).apply_rotation(Rot::Left(6)).value());
+        assert_eq!(19, Safe::new(11).apply_rotation(Rot::Right(8)).current);
+        assert_eq!(5, Safe::new(11).apply_rotation(Rot::Left(6)).current);
         assert_eq!(
             0,
-            Safe(11)
+            Safe::new(11)
                 .apply_rotation(Rot::Right(8))
                 .apply_rotation(Rot::Left(19))
-                .value()
+                .current
         );
-        assert_eq!(99, Safe(0).apply_rotation(Rot::Left(1)).value());
-        assert_eq!(0, Safe(99).apply_rotation(Rot::Right(1)).value());
-        assert_eq!(50, Safe(50).apply_rotation(Rot::Left(0)).value());
-        assert_eq!(50, Safe(50).apply_rotation(Rot::Right(0)).value());
+        assert_eq!(99, Safe::new(0).apply_rotation(Rot::Left(1)).current);
+        assert_eq!(0, Safe::new(99).apply_rotation(Rot::Right(1)).current);
+        assert_eq!(50, Safe::new(50).apply_rotation(Rot::Left(0)).current);
+        assert_eq!(50, Safe::new(50).apply_rotation(Rot::Right(0)).current);
     }
 
     #[test]
     fn test_part_1_example() {
         let rots = parse_rot_seq(DEMO_INPUT);
-        assert_eq!(3, count_state_reached(Safe(50), rots, 0))
+        assert_eq!(3, count_state_reached(Safe::new(50), &rots, 0));
+    }
+
+    #[test]
+    fn test_part_2_example() {
+        let rots = parse_rot_seq(DEMO_INPUT);
+        assert_eq!(6, Safe::new(50).apply_rotations(&rots).clicks);
+    }
+
+    #[test]
+    fn test_big_rotation_clicks() {
+        let res = Safe::new(50).apply_rotation(Rot::Right(1000));
+        assert_eq!(50, res.current);
+        assert_eq!(10, res.clicks);
     }
 }
