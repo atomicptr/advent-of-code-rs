6e60e600c4806c541221e05cf68bfe3b23761283
Christopher Kaster <me@atomicptr.de>
2025-12-07 16:48:50 +0100

    add too slow solution for 2025-07 part 2

--- 2025/07/main.rs
+++ 2025/07/main.rs
@@ -5,9 +5,13 @@ fn main() {
     m.finish();
 
     println!("Part 1 result is {}", m.num_splits);
+
+    let m = Machine::from(input);
+
+    println!("Part 2 result is {}", m.finish_timelines());
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Cell {
     Start,
     Empty,
@@ -40,6 +44,7 @@ impl Cell {
     }
 }
 
+#[derive(Clone)]
 struct Machine {
     grid: Vec<Cell>,
     width: usize,
@@ -96,6 +101,91 @@ impl Machine {
         assert!(matches!(self.grid.get(index).unwrap(), Cell::Empty));
 
         self.grid[index] = cell;
+    }
+
+    fn clone_with_new_beam(&self, (x, y): (usize, usize)) -> Self {
+        let index = self.index(x, y);
+        let mut new = self.clone();
+
+        assert!(matches!(new.grid[index], Cell::Empty));
+        new.grid[index] = Cell::Beam;
+        new.beams.clear();
+        new.beams.push((x, y));
+        new.num_splits = 0;
+
+        new
+    }
+
+    fn split_timeline(mut self) -> Vec<Self> {
+        let mut timelines = vec![];
+        let mut new_beams = vec![];
+
+        for (beam_x, beam_y) in &self.beams {
+            if beam_y + 1 >= self.height() {
+                continue;
+            }
+
+            let below_beam = self.index(beam_x.clone(), beam_y + 1);
+
+            let beam_y = beam_y + 1;
+
+            match self.grid.get(below_beam) {
+                Some(Cell::Empty) => new_beams.push((*beam_x, beam_y)),
+                Some(Cell::Splitter) => {
+                    // left beam
+                    if let Some(new_beam_x) = beam_x.checked_sub(1) {
+                        if let Some(cell) = self.grid.get(self.index(new_beam_x, beam_y))
+                            && matches!(cell, Cell::Empty)
+                        {
+                            timelines.push(self.clone_with_new_beam((new_beam_x, beam_y)));
+                        }
+                    }
+
+                    // right beam
+                    if let Some(new_beam_x) = beam_x.checked_add(1) {
+                        if let Some(cell) = self.grid.get(self.index(new_beam_x, beam_y))
+                            && matches!(cell, Cell::Empty)
+                        {
+                            timelines.push(self.clone_with_new_beam((new_beam_x, beam_y)));
+                        }
+                    }
+                }
+                _ => {
+                    continue;
+                }
+            }
+        }
+
+        // make sure we dont have duplicates
+        let new_beams: Vec<_> = new_beams
+            .iter()
+            .cloned()
+            .collect::<std::collections::HashSet<_>>()
+            .into_iter()
+            .collect();
+
+        for (beam_x, beam_y) in &new_beams {
+            let index = self.index(*beam_x, *beam_y);
+            if let Some(cell) = self.grid.get(index) {
+                assert!(
+                    matches!(cell, Cell::Empty),
+                    "Expected empty at {beam_x}x{beam_y} but found {cell:?}"
+                );
+
+                self.grid[index] = Cell::Beam;
+            }
+        }
+
+        let timeline_still_alive = new_beams.len() > 0;
+
+        self.beams.extend(new_beams);
+
+        // continue our timeline if something happened
+        if timeline_still_alive {
+            timelines.push(self);
+        }
+
+        timelines
     }
 
     fn step(&mut self) -> bool {
@@ -174,6 +264,31 @@ impl Machine {
 
     fn finish(&mut self) {
         while self.step() {}
+    }
+
+    fn finish_timelines(self) -> usize {
+        let mut active = vec![self];
+        let mut dead = 0;
+
+        while !active.is_empty() {
+            let mut next = Vec::new();
+
+            for timeline in active {
+                let branches = timeline.split_timeline();
+
+                if branches.is_empty() {
+                    dead += 1;
+                }
+
+                for branch in branches {
+                    next.push(branch);
+                }
+            }
+
+            active = next;
+        }
+
+        dead
     }
 
     fn to_string(&self) -> String {
@@ -357,5 +472,11 @@ mod test {
         m.finish();
 
         assert_eq!(21, m.num_splits);
+    }
+
+    #[test]
+    fn test_timelines() {
+        let m = Machine::from(DEMO_INPUT);
+        assert_eq!(40, m.finish_timelines());
     }
 }
